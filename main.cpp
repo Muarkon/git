f98dc1cff1626a02d42256b9cdd28e922d688a51
Muarkon <3759451824@qq.com>
2026-01-17 10:33:59 +0800

    first save

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void solve();
+int main() {
+	ios::sync_with_stdio(0), cout.tie(0), cin.tie(0);
+	int t = 1;
+	while (t--)
+		solve();
+	return 0;
+}
+void solve() {
+	cout << "Hello, World!" << '\n';
+}
